4fd1ed5a2fafe988c3f29d2c8009f41a9800016f
Vicky Kumar <kumar.v005@gmail.com>
2021-09-25 23:31:30 +0530

    backend deploy

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/album', {
+mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
