0ecaf8f824b28b7147cbe66c498c5c03d1a6e15d
Vicky Kumar <kumar.v005@gmail.com>
2021-09-25 23:28:13 +0530

    backend deploy

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/album', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/album', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require("cors");
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({
     extended: true
 }));
+const corsOpts = {
+    origin: "*",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type"],
+};
+app.use(cors(corsOpts));
 const dbConfig = require('./config/dbConfig');
 app.use(express.static('images'));
 const PORT = process.env.PORT || 5000;
